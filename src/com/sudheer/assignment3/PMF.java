2e20e4fea2b93eaddd6372815b72a20ca2e24c4c
Sudhaeer <sudheer.aluru007@gmail.com>
2016-04-30 18:47:12 +0530

    Milestone 1: Add the standard PMF.java

---
+++ src/com/sudheer/assignment3/PMF.java
@@ -0,0 +1,23 @@
+// PersistenceManagerFactory.java
+//
+// A standard java class in GAE that is used to manage JDO objects
+package com.sudheer.assignment3;
+//imports
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManagerFactory;
+
+//class definition
+public final class PMF {
+	// instance of the factory manager
+	private static final PersistenceManagerFactory pmf_instance = JDOHelper
+			.getPersistenceManagerFactory("transactions-optional");
+
+	// private constructor such that nothing can make an instance of this //
+	// class
+	private PMF() {
+	}
+	// returns the static instance of the factory manager
+	public static PersistenceManagerFactory get() {
+		return pmf_instance;
+	}
+}
