1468c8e82a9eb8d1f12ae3f43728969113db6160
Sudhaeer <sudheer.aluru007@gmail.com>
2016-04-30 16:54:36 +0530

    Base Version

---
+++ src/com/sudheer/assignment3/PMF.java
@@ -0,0 +1,20 @@
+package com.sudheer.assignment3;
+//imports
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManagerFactory;
+
+//class definition
+public final class PMF {
+	// instance of the factory manager
+	private static final PersistenceManagerFactory pmf_instance = JDOHelper
+			.getPersistenceManagerFactory("transactions-optional");
+
+	// private constructor such that nothing can make an instance of this //
+	// class
+	private PMF() {
+	}
+	// returns the static instance of the factory manager
+	public static PersistenceManagerFactory get() {
+		return pmf_instance;
+	}
+}
